4965fd483ee5966f8e7b2e717b283eef806c3a61
asds <asd>
2020-08-17 00:20:07 +0500

    initial git

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuToggle() {
+    var nav = document.getElementById
+        ('menu-overlay');
+    nav.classList.toggle('active');
+    var nav = document.getElementById
+        ('toggleIcon');
+    nav.classList.toggle('active');
+}
